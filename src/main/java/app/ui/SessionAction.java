ba54ea3c70e2ac04ff33c77b97a3af4ba9d1ee02
Glueckseule <30928455+Glueckseule@users.noreply.github.com>
2020-01-14 17:30:24 +0100

    StatusBar simple  message added

--- src/main/java/app/ui/SessionAction.java
+++ src/main/java/app/ui/SessionAction.java
@@ -1,16 +1,20 @@
 package app.ui;
 
+import app.App;
 import app.services.application.ApplicationService;
-import app.services.application.ApplicationState;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.components.ServiceManager;
+import com.intellij.openapi.wm.StatusBar;
+import com.intellij.openapi.wm.WindowManager;
 import org.jetbrains.annotations.NotNull;
 
 public class SessionAction extends AnAction {
 
     private static final String START_SESSION_TEXT = "Start Session";
     private static final String SAVE_SESSION_TEXT = "Save Session";
+    private static final String SESSION_RUNNING_TEXT = "BI Plugin is running";
+    private static final String SESSION_STOPPED_TEXT = "Thanks for participating";
 
     private ApplicationService applicationService;
 
@@ -20,17 +24,32 @@ public class SessionAction extends AnAction {
 
     @Override
     public void actionPerformed(@NotNull AnActionEvent e) {
-        ApplicationState currentState = applicationService.getState();
         switch (applicationService.getState()) {
             case IDLE:
                 applicationService.startSession();
+                setStatusBarState(true);
                 e.getPresentation().setText(SAVE_SESSION_TEXT);
                 break;
             case RECORDING:
                 applicationService.saveSession();
+                setStatusBarState(false);
                 e.getPresentation().setText(START_SESSION_TEXT);
                 break;
         }
+
+    }
+
+    //show hint in statusbar when the plugin is running
+    private void setStatusBarState(Boolean isRunning) {
+        StatusBar statusBar = WindowManager.getInstance().getStatusBar(App.getCurrentProject());
+
+        if (statusBar != null && isRunning) {
+            statusBar.setInfo(SESSION_RUNNING_TEXT);
+        } else {
+            statusBar.setInfo(SESSION_STOPPED_TEXT);
+        }
+
+
     }
 
 }
